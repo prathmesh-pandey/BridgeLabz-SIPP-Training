17beeda5a29095e9f7bf784f2987d26ddeb40652
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-06-26 12:49:54 +0530

    Prathmesh Pandey Added Fully Working Code

---
+++ Day2_ControlFlow/PowerWhileLoop.java
@@ -0,0 +1,23 @@
+package LocalRepo.Day2_ControlFlow;
+
+import java.util.Scanner;
+
+public class PowerWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter base number: ");
+        int number = scanner.nextInt();
+        System.out.print("Enter power: ");
+        int power = scanner.nextInt();
+
+        int result = 1;
+        int counter = 0;
+
+        while (counter < power) {
+            result *= number;
+            counter++;
+        }
+
+        System.out.println(number + " raised to the power " + power + " is " + result);
+    }
+}
