eb0b7b5a761c744775cc39ce264279851cde5464
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-06-26 11:58:14 +0530

    Prathmesh Pandey Added Fully Working Code

---
+++ Day2_ControlFlow/GreatestFactor.java
@@ -0,0 +1,25 @@
+package LocalRepo.Day2_ControlFlow;
+
+import java.util.Scanner;
+
+public class GreatestFactor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int greatestFactor = 1;
+        int counter = number - 1;
+
+        while (counter > 0) {
+            if (number % counter == 0) {
+                greatestFactor = counter;
+                break;
+            }
+            counter--;
+        }
+
+        System.out.println("Greatest factor (excluding itself): " + greatestFactor);
+    }
+}
+
