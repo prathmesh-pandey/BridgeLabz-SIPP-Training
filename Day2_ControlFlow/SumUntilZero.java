35ecc3fddda0f636e7e174a005f2c758001d4603
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-06-28 17:20:10 +0530

    Prathmesh Pandey AddedFully Working Code

---
+++ Day2_ControlFlow/SumUntilZero.java
@@ -0,0 +1,15 @@
+package LocalRepo.Day2_ControlFlow;
+
+import java.util.Scanner;
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;
+        double num = sc.nextDouble();
+        while (num != 0) {
+            total = total + num;
+            num = sc.nextDouble();
+        }
+        System.out.println("Total sum is " + total);
+    }
+}
