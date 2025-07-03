7d77d4bd5ce6820fd1c61d5c886da466ac2b006d
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-07-03 08:40:00 +0530

    Prathmesh Pandey AddedFully Working Code

---
+++ Inheritance/SmartHomeDevices.java
@@ -0,0 +1,39 @@
+public class SmartHomeDevices {
+    public static void main(String[] args) {
+        Thermostat myThermostat = new Thermostat("T1234", "On", 24.5);
+
+        System.out.println("🏠 Smart Home Device Info:");
+        myThermostat.displayStatus();
+    }
+}
+
+class Device {
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+
+class Thermostat extends Device {
+    double temperatureSetting;
+
+    Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
