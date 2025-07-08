8228664f658593eeef35ecd900b23565b02ca231
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-07-08 09:14:15 +0530

    Prathmesh Pandey Added Fully Working Code

---
+++ Day5_Strings/NumberFormatDemo.java
@@ -0,0 +1,28 @@
+package LocalRepo.Day5_Strings;
+
+import java.util.Scanner;
+
+public class NumberFormatDemo {
+    public static void generateException(String text) {
+        int num = Integer.parseInt(text);
+        System.out.println(num);
+    }
+
+    public static void handleException(String text) {
+        try {
+            int num = Integer.parseInt(text);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException");
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        handleException(text);
+    }
+}
+
