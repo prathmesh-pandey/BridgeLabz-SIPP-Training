37fc374161df5f02e41bb1c2a6fcd31f19fee65b
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-07-08 09:12:15 +0530

    Prathmesh Pandey Added Fully Working Code

---
+++ Day5_Strings/NullPointerDemo.java
@@ -0,0 +1,22 @@
+package LocalRepo.Day5_Strings;
+
+public class NullPointerDemo {
+    public static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    public static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException");
+        }
+    }
+
+    public static void main(String[] args) {
+        handleException();
+    }
+}
+
